3255b42f50bbac112d4e5c9a105ffb33baa1381e
Maciej Grela <enki@fsck.pl>
2023-06-07 20:35:40 +0200

    Make the tree compatible with BR2_EXTERNAL

---
+++ external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_DLINK_DNS320L_PATH)/package/*/*.mk))
--- package/dns320l-daemon/dns320l-daemon.mk
+++ package/dns320l-daemon/dns320l-daemon.mk
@@ -7,36 +7,27 @@
 DNS320L_DAEMON_VERSION = 1.0
 DNS320L_DAEMON_SOURCE = tip.tar.bz2
 DNS320L_DAEMON_SITE = https://www.aboehler.at/hg/dns320l-daemon/archive
-DNS320L_DAEMON_INSTALL_STAGING = YES
 DNS320L_DAEMON_LICENSE = GPL-3
 DNS320L_DAEMON_DEPENDENCIES = iniparser
 
-DNS320L_DAEMON_CONFIGURE_OPTS = $(TARGET_CONFIGURE_OPTS)
 
 define DNS320L_DAEMON_BUILD_CMDS
-	$(DNS320L_DAEMON_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
-endef
-
-define DNS320L_DAEMON_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/dns320l-daemon $(STAGING_DIR)/usr/bin/
-	$(INSTALL) -D -m 0600 $(@D)/dns320l-daemon.ini $(STAGING_DIR)/etc/
-	$(INSTALL) -D -m 0644 $(@D)/dns320l-daemon.service $(STAGING_DIR)/lib/systemd/system/
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) \
+		CFLAGS="$(TARGET_CFLAGS) -D_GNU_SOURCE"
 endef
 
 define DNS320L_DAEMON_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/dns320l-daemon $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D -m 0600 $(@D)/dns320l-daemon.ini $(TARGET_DIR)/etc/
+endef
+
+define DNS320L_DAEMON_INSTALL_INIT_SYSTEMD
 	$(INSTALL) -D -m 0644 $(@D)/dns320l-daemon.service $(TARGET_DIR)/lib/systemd/system/
 endef
 
-define HOST_DNS320L_DAEMON_BUILD_CMDS
-	$(HOST_CONFIGURE_OPTS) $(HOST_MAKE_ENV) $(MAKE) -C $(@D)
-endef
-
-define HOST_DNS320L_DAEMON_INSTALL_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/dns320l-daemon $(HOST_DIR)/usr/bin/
-	$(INSTALL) -D -m 0600 $(@D)/dns320l-daemon.ini $(HOST_DIR)/etc/
+define DNS320L_DAEMON_INSTALL_INIT_SYSV
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_DLINK_DNS320L_PATH)/package/dns320l-daemon/S50dns320l-daemon $(TARGET_DIR)/etc/init.d/S50dns320l-daemon
 endef
 
 $(eval $(generic-package))
-$(eval $(host-generic-package))
+
